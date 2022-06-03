a083803a80c4320fa9d5f4bedb42cf3e73c63c85
yossu-y <yangyuub@gmail.com>
2022-06-03 09:24:02 +0000

    [update]article-controller

--- app/controllers/public/articles_controller.rb
+++ app/controllers/public/articles_controller.rb
@@ -1,13 +1,59 @@
 class Public::ArticlesController < ApplicationController
-  def index
-  end
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def new
+    @article = Article.new
   end
-
-  def show
+  
+  def index
+    @articles = Article.all
+    @article = Article.new
   end
 
   def edit
+    @article = Article.find(params[:id])
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def create
+    @article = current_user.articles.new(article_params)
+    @article.user_id = current_user.id
+    if @article.save
+      redirect_to articles_path
+    else
+      @articles = Article.all
+      render "new"
+    end
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render "edit"
+    end
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+  
+  private
+  
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+  
+  def correct_user
+    @article = Article.find(params[:id])
+    @user = @article.user
+    redirect_to(articles_path) unless @user == current_user
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  
 end
