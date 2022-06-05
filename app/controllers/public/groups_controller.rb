780f0f0a7fe34303fea695cd78f6f67588c9a56c
yossu-y <yangyuub@gmail.com>
2022-06-05 06:38:15 +0000

    [update]groups-controller

--- app/controllers/public/groups_controller.rb
+++ app/controllers/public/groups_controller.rb
@@ -3,28 +3,51 @@ class Public::GroupsController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update]
   
   def index
+    @article = Article.new
+    @groups = Group.all
   end
   
   def show
+    @article = Article.new
+    @group = Group.find(params[:id])
   end
   
   def new
+    @group = Group.new
   end
   
   def create
+    @group = Group.new(group_params)
+    @group.owner_id = current_user.id
+    if @group.save
+      redirect_to groups_path
+    else
+      render "new"
+    end
   end
   
   def edit
   end
   
   def update
+    if @group.update(group_params)
+      redirect_to groups_path
+    else
+      render "edit"
+    end
   end
   
   private
   
   def group_params
+    params.require(:group).permit(:name, :introduction)
   end
   
   def ensure_correct_user
+    @group = Group.find(params[:id])
+    unless @group.owner.id == current_user.id
+      redirect_to groups_path
+    end
   end
+  
 end
