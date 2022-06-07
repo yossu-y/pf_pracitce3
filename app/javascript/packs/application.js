421e8a8154566a480628e37b304dc35f12d1c661
yossu-y <yangyuub@gmail.com>
2022-06-07 07:30:01 +0000

    [add]javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
 import "jquery";
+import "skippr.min"
+import "script.js"
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application" 
