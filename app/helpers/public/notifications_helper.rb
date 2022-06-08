5df85cff3a0bfc7aee965a6fe5e8bd24b897a938
yossu-y <yangyuub@gmail.com>
2022-06-08 07:32:05 +0000

    [update]layouts

--- app/helpers/public/notifications_helper.rb
+++ app/helpers/public/notifications_helper.rb
@@ -3,4 +3,21 @@ module Public::NotificationsHelper
   def unchecked_notifications
     @notifications = current_user.passive_notifications.where(checked: false)
   end
+  
+  # def notifications_form(notifications)
+  #   article = notifications.article
+  #   your_post = link_to "あなたの投稿", @notifications.article, remote: true
+  #   case notification.action
+  #   when "comment"
+  #     "があなたの投稿にコメントをしました#{time_ago_in_words(notification.created_at)}前)"
+  #   when "like"
+  #     "が#{your_post}にいいね!しました#{time_ago_in_words(notification.created_at)}前)"
+  #   when "follow"
+  #     "があなたをフォローしました#{time_ago_in_words(notification.created_at)}前)"
+  #   else
+  #     何かアクションをした
+  #   end
+
+  # end
+  
 end
